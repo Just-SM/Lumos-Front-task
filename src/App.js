e402a193d6e1110fca053641cb9fa1dba1dd9618
Roman <romafurzykov@gmail.com>
2025-04-09 14:16:38 +0200

    init push

--- src/App.js
+++ src/App.js
@@ -14,27 +14,9 @@ function App() {
       alert('Please enter a valid item.');
       return;
     }
-    
-    // Check for duplicate items
-    if (items.includes(inputValue)) {
-      alert('This item already exists in the list.');
-      return;
-    }
 
     setItems([...items, inputValue]);
     setInputValue('');
-  };
-
-
-  // Function to handle pressing "Enter" to add items
-  const handleKeyPress = (e) => {
-    if (e.key === 'Enter') {
-      handleAddItem(); // Call add item function when "Enter" is pressed
-    }
-  };
-
-  const handleRemoveItem = () => {
-    setItems(items.slice(0,-1));
   };
 
   return (
@@ -50,12 +32,9 @@ function App() {
         value={inputValue}
         onChange={(e) => setInputValue(e.target.value)} 
 
-        // add event listener
-        onKeyDown ={handleKeyPress}
       />
       <button onClick={handleAddItem}>Add Item</button>
     
-      <button onClick={handleRemoveItem}>Remove Item</button>
     </div>
   );
 }
